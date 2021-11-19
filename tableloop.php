bd0ec40fade72282ba6e508561aeb84e4107ab1c
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2021-11-19 14:39:12 +0700

    Update tableloop.php

--- tableloop.php
+++ tableloop.php
@@ -6,7 +6,12 @@
       <?php
         for($i=0;$i<100;$i++){
           echo "<tr>";
-          echo "<td>คอลัมน์=6</td>";
+          echo "<td>คอลัมน์<td>";
+          echo "<td>คอลัมน์<td>";
+          echo "<td>คอลัมน์<td>";
+          echo "<td>คอลัมน์<td>";
+          echo "<td>คอลัมน์<td>";
+          echo "<td>คอลัมน์</td>";
           echo "</tr>";
         }
       ?>
