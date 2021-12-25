842596200bf2c63689895f4ec3b5a8d2fddbb44e
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2021-12-25 20:37:11 +0700

    Update midterm.php

--- midterm.php
+++ midterm.php
@@ -1,7 +1,7 @@
 <?php
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
-  echo "<tr align = 'center'>";
+
   echo "<table border='1'>";
   echo"<caption>";
   echo "<h3>";
