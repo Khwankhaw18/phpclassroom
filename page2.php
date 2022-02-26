37b5869fdb66526b6254dd44586f29d537f2c76f
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2022-02-26 19:03:27 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,22 +1,67 @@
-<body style="background-color:#ffd3b2"> 
-    <div class="container"> <!-- คอนเทนเนอร์ -->
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Suphan Buri Project</title>
+    
+    <style>
+      #header{
+        height: 120px;
+        background-color: #80B7A2;
+      }
+      #sidebar{
+        height: 0px;
+        background-color: #ADD495;
+        padding: 0px;
+      }
+      #content{
+        height: 0px;
+        background-color: #F6F5F0;
+      }
+      ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        background-color: #FFF8F8;
+      }
+      li a {
+        display: block;
+        color: #000;
+        padding: 8px 16px;
+        text-decoration: none;
+      }
+      /* Change the link color on hover */
+      li a:hover {
+        background-color: #555;
+        color: #FFFFFF;
+      }
+    </style>
+    
+  </head>
+  <body> 
+    <div class="container">
       <div class="row">
-        <div class="col-12" id="header"> <!-- เฮดเดอร์ -->
-          <h1>จังหวัดนครศรีธรรมราช</h1>
-          <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nakhon Si Thammarat</h3>
+        <div class="col-12" id="header"align="center">
+          <h1>จังหวัดสุพรรณบุรี</h1>
+          <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Suphan Buri</h3>
         </div>
       </div>
       <div class="row">
-        <div class="col-3" id="sidebar"> <!-- ไซด์บาร์ -->
+        <div class="col-3" id="sidebar"> 
           <ul>
-            <li><a href="https://lw-58513.herokuapp.com/">หน้าแรก</a></li>
-            <li><a href="https://lw-58513.herokuapp.com/page=1.php">แหล่งท่องเที่ยว</a></li> 
-            <li><a href="https://lw-58513.herokuapp.com/page=2.php">ประเพณีและวัฒนธรรม</a></li> 
-            <li><a href="https://lw-58513.herokuapp.com/page=3.php">อาหารประจำท้องถิ่น</a></li> 
-            <li><a href="https://lw-58513.herokuapp.com/page=4.php">ผู้จัดทำ</a></li>
-          </ul>
+            <li><a href="https://lsw-56411.herokuapp.com/website.php">หน้าแรก</a></li>
+            <li><a href="https://lsw-56411.herokuapp.com/page1.php">แหล่งท่องเที่ยว</a></li> 
+            <li><a href="https://lsw-56411.herokuapp.com/page2.php">ประเพณีและวัฒนธรรม</a></li> 
+            <li><a href="https://lsw-56411.herokuapp.com/page3.php">อาหารประจำท้องถิ่น</a></li> 
+            <li><a href="https://lsw-56411.herokuapp.com/page4.php">ผู้จัดทำ</a></li>
+         </ul>
        </div>
-        <div class="col-9" id="content"> <!-- คอนเท้นต์ -->
+        <div class="col-9" id="content"> 
         <h5>
           ประเพณีและวัฒนธรรม
           </h5><br>
