812992ab77d6fc8544ba0047434cdd817cad7784
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2022-02-26 16:03:36 +0700

    Update and rename website.html to website.php

---
+++ website.php
@@ -0,0 +1,202 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Nakhon Si Thammarat Project</title>
+    
+    <style>
+      #header{
+        height: 120px;
+        background-color: #FFA500;
+      }
+      #sidebar{
+        height: 0px;
+        background-color: #f1f1f1;
+        padding: 0px;
+      }
+      #content{
+        height: 0px;
+        background-color: #F5F5F5;
+      }
+      ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        background-color: #f1f1f1;
+      }
+      li a {
+        display: block;
+        color: #000;
+        padding: 8px 16px;
+        text-decoration: none;
+      }
+      /* Change the link color on hover */
+      li a:hover {
+        background-color: #555;
+        color: #FFFFFF;
+      }
+    </style>
+    
+  </head>
+  <body> 
+    <div class="container">
+      <div class="row">
+        <div class="col-12" id="header">
+          <h1>จังหวัดนครศรีธรรมราช</h1>
+          <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nakhon Si Thammarat</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3" id="sidebar"> 
+          <ul>
+            <li><a href="https://lw-58513.herokuapp.com/">หน้าแรก</a></li>
+            <li><a href="https://lw-58513.herokuapp.com/page=1.php">แหล่งท่องเที่ยว</a></li> 
+            <li><a href="https://lw-58513.herokuapp.com/page=2.php">ประเพณีและวัฒนธรรม</a></li> 
+            <li><a href="https://lw-58513.herokuapp.com/page=3.php">อาหารประจำท้องถิ่น</a></li> 
+            <li><a href="https://lw-58513.herokuapp.com/page=4.php">ผู้จัดทำ</a></li>
+         </ul>
+       </div>
+        <div class="col-9" id="content"> 
+         <h4>
+           นครศรีธรรมราช
+         </h4>
+          <table widht="640" height="440" align="center">
+          <tr>
+           <td>
+             <img src="P1.png">
+           </td> 
+          </tr>
+         </table>
+            <h5>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;เป็นจังหวัดในประเทศไทย มีประชากรมากที่สุดในภาคใต้และมีขนาดพื้นที่ใหญ่เป็นอันดับ 2 ของภาคใต้ (รองจากสุราษฎร์ธานี) เป็นจังหวัดที่มีอำเภอมากที่สุดในภาคใต้ ห่างจากกรุงเทพมหานคร ประมาณ 780 กิโลเมตร มีจังหวัดที่อยู่ติดกันได้แก่ สงขลา พัทลุง ตรัง กระบี่ และสุราษฎร์ธานี
+            </h5>
+           <br></br>
+          <h4>
+           ที่ตั้งและอาณาเขต
+         </h4>
+          <h5>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;จังหวัดนครศรีธรรมราชตั้งอยู่ฝั่งตะวันออกของภาคใต้ โดยมีพื้นที่ใหญ่เป็นอันดับ 2 ของภาคใต้ โดยมีจังหวัดที่มีอาณาเขตติดกัน ดังนี้
+          </h5>
+          <h5>
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ด้านเหนือ ติดกับจังหวัดสุราษฎร์ธานี และอ่าวไทย
+          </h5>
+          <h5>
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ด้านใต้ ติดกับจังหวัดตรัง จังหวัดพัทลุง และจังหวัดสงขลา
+          </h5>
+          <h5>
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ด้านตะวันออก ติดกับอ่าวไทย
+          </h5>
+          <h5>
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ด้านตะวันตก ติดกับจังหวัดสุราษฎร์ธานี และจังหวัดกระบี่
+          </h5>
+          <h5>
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;พื้นที่ที่ติดกับทะเลฝั่งอ่าวไทยของจังหวัดนครศรีธรรมราชนั้นมีชายฝั่งยาวประมาณ 225 กิโลเมตร และจังหวัดนครศรีธรรมราชมีเขาสูงที่สุดในภาคใต้ คือ เขาหลวง มีความสูงประมาณ 1,835 เมตร
+          </h5>
+          <br></br>
+          <h5 align="center">แผนที่จังหวัด</h5>
+          
+          <table widht="300" height="400" align="center">
+          <tr>
+           <td>
+             <img src="map.png">
+           </td> 
+          </tr>
+         </table> 
+        <br>
+          <h6 align="center">
+            การปกครองส่วนภูมิภาค
+          </h6>
+          <table>
+            <tbody>
+              <tr valign ="top">
+          <td>
+            <ol>
+              <li>
+                อำเภอเมืองนครศรีธรรมราช
+                </li>
+              <li>
+                อำเภอพรหมคีรี
+                </li>
+              <li>
+                อำเภอลานสกา
+              </li>
+              <li>
+                อำเภอฉวาง
+                </li>
+              <li>    
+                อำเภอพิปูน
+                </li>
+                <li>
+                อำเภอเชียรใหญ่
+                    </li>
+              <li>  
+                อำเภอชะอวด
+                  </li>
+              <li>         
+                อำเภอท่าศาลา          
+                  </li>
+              <li>       
+                อำเภอทุ่งสง         
+                  </li>
+              <li>  
+                อำเภอนาบอน         
+                  </li>
+              <li>             
+                อำเภอทุ่งใหญ่
+           
+                  </li>
+              <li>     
+                อำเภอปากพนัง
+                  </li>
+            </ol>
+      </td>
+      <td>
+            <ol start="13">
+                   <li>      
+                อำเภอร่อนพิบูลย์          
+                </li>
+              <li>         
+                อำเภอสิชล       
+                </li>
+              <li>
+                อำเภอขนอม
+              </li>
+              <li>
+                อำเภอหัวไทร   
+                </li>
+              <li>    
+                อำเภอบางขัน         
+                </li>
+                <li>
+                อำเภอถ้ำพรรณรา       
+                    </li>
+              <li>           
+                อำเภอจุฬาภรณ์     
+                  </li>
+              <li>  
+                 อำเภอพระพรหม    
+                  </li>
+              <li>  
+                อำเภอนบพิตำ
+                  </li>
+              <li>
+                อำเภอช้างกลาง    
+                  </li>
+              <li>                
+                อำเภอเฉลิมพระเกียรติ
+                  </li>
+            </ol>
+      </td>
+              </tr>
+            </tbody>
+      </table>
+      </div>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
