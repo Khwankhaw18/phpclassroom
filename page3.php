88b19c5ef4f91f31799fc68cc1a1eba403739f46
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2022-02-26 17:26:55 +0700

    Create page3.php

---
+++ page3.php
@@ -0,0 +1,81 @@
+<div class="container"> <!-- คอนเทนเนอร์ -->
+      <div class="row">
+        <div class="col-12" id="header"> <!-- เฮดเดอร์ -->
+          <h1>จังหวัดนครศรีธรรมราช</h1>
+          <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nakhon Si Thammarat</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3" id="sidebar"> <!-- ไซด์บาร์ -->
+          <ul>
+            <li><a href="https://lw-58513.herokuapp.com/">หน้าแรก</a></li>
+            <li><a href="https://lw-58513.herokuapp.com/page=1.php">แหล่งท่องเที่ยว</a></li> 
+            <li><a href="https://lw-58513.herokuapp.com/page=2.php">ประเพณีและวัฒนธรรม</a></li> 
+            <li><a href="https://lw-58513.herokuapp.com/page=3.php">อาหารประจำท้องถิ่น</a></li> 
+            <li><a href="https://lw-58513.herokuapp.com/page=4.php">ผู้จัดทำ</a></li>
+          </ul>
+       </div>
+        <div class="col-9" id="content"> <!-- คอนเท้นต์ -->
+        <h5>
+          อารหารประจำท้องถิ่น
+          </h5><br>
+          <h5>
+            1.ขนมจีนน้ำยาเส้นสด
+          </h5><br>
+          <table widht="500" height="300" align="center">
+          <tbody><tr>
+           <td>
+             <img src="P12.png">
+           </td> 
+          </tr>
+         </tbody></table><br>
+          <h5>
+            2.ข้าวยำ
+          </h5><br>
+              <table widht="500" height="300" align="center">
+          <tbody><tr>
+           <td>
+             <img src="P13.png">
+           </td> 
+          </tr>
+         </tbody></table><br>
+         
+          <h5>
+            3.แกงส้ม
+            </h5><br>
+              <table widht="500" height="300" align="center">
+          <tbody><tr>
+           <td>
+             <img src="P14.png">
+           </td> 
+          </tr>
+         </tbody></table><br>
+          
+          <h5>
+            4.คั่วกลิ้ง
+          </h5><br>
+              <table widht="500" height="300" align="center">
+          <tbody><tr>
+           <td>
+             <img src="P15.png">
+           </td> 
+          </tr>
+         </tbody></table><br>
+         
+          <h5>
+            5.แกงไตปลา
+          </h5><br>
+              <table widht="500" height="300" align="center">
+          <tbody><tr>
+           <td>
+             <img src="P16.png">
+           </td> 
+          </tr>
+         </tbody></table><br>       
+        </div>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  
+
+</div></div>
