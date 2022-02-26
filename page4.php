9bbd0fd2167f6b3701ab1025fce578390f176ef1
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2022-02-26 17:45:36 +0700

    Create page4.php

---
+++ page4.php
@@ -0,0 +1,94 @@
+<body style="background-color:#ffd3b2"> 
+    <div class="container"> <!-- คอนเทนเนอร์ -->
+      <div class="row">
+        <div class="col-12" id="header"> <!-- เฮดเดอร์ -->
+          <h1>จังหวัดนครศรีธรรมราช</h1>
+          <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nakhon Si Thammarat</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3" id="sidebar"> <!-- ไซด์บาร์ -->
+          <ul>
+            <li><a href="https://lw-58513.herokuapp.com/">หน้าแรก</a></li>
+            <li><a href="https://lw-58513.herokuapp.com/page=1.php">แหล่งท่องเที่ยว</a></li> 
+            <li><a href="https://lw-58513.herokuapp.com/page=2.php">ประเพณีและวัฒนธรรม</a></li> 
+            <li><a href="https://lw-58513.herokuapp.com/page=3.php">อาหารประจำท้องถิ่น</a></li> 
+            <li><a href="https://lw-58513.herokuapp.com/page=4.php">ผู้จัดทำ</a></li>
+          </ul>
+       </div>
+        <div class="col-9" id="content"> <!-- คอนเท้นต์ -->
+        <h5>
+          ที่เที่ยวนครศรีธรรมราช
+          </h5><br>
+          <h5>
+            1.นายธีรพัฒน์ คำมา ม.5/13 เลขที่ 6
+          </h5><br>
+          <table widht="500" height="300" align="center">
+          <tbody><tr>
+           <td>
+             <img src="ธร.png">
+           </td> 
+          </tr>
+         </tbody></table><br>
+          
+          <h5>
+            2.นายปิยโชติ จันทร์สุริยะ ม.5/13 เลขที่ 7
+          </h5><br>
+              <table widht="500" height="300" align="center">
+          <tbody><tr>
+           <td>
+             <img src="ปช.png">
+           </td> 
+          </tr>
+         </tbody></table><br>
+          
+          <h5>
+            3.นายพีรณัฐ อุตราชา ม.5/13 เลขที่ 8
+            </h5><br>
+              <table widht="500" height="300" align="center">
+          <tbody><tr>
+           <td>
+             <img src="พร.png">
+           </td> 
+          </tr>
+         </tbody></table><br>
+         
+          <h5>
+            4.นายศุภโชค ขจรศักดา ม.5/13 เลขที่ 9
+          </h5><br>
+              <table widht="500" height="300" align="center">
+          <tbody><tr>
+           <td>
+             <img src="ชช.png">
+           </td> 
+          </tr>
+         </tbody></table><br>
+         
+          <h5>
+            5.นายสารัช สารีคำ ม.5/13 เลขที่ 10
+          </h5><br>
+              <table widht="500" height="300" align="center">
+          <tbody><tr>
+           <td>
+             <img src="สร.png">
+           </td> 
+          </tr>
+         </tbody></table><br>
+          
+           <h5>
+            6.นายชนาเดช กุลกรอม ม.5/13 เลขที่ 11
+          </h5><br>
+              <table widht="500" height="300" align="center">
+          <tbody><tr>
+           <td>
+             <img src="ชน.png">
+           </td> 
+          </tr>
+         </tbody></table><br>  
+        </div>
+     
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  
+
+</div></div></body>
