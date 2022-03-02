c0956b90703e0159a5eca3484a3af7fcca91d648
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2022-03-02 16:37:21 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -7,7 +7,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Suphan Buri Project</title>
-    
+   
     <style>
       #header{
         height: 120px;
@@ -47,18 +47,18 @@
     <div class="container">
       <div class="row">
         <div class="col-12" id="header"align="center">
-          <h1>จังหวัดสุพรรณบุรี</h1>
-          <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Suphan Buri</h3>
+          <h1><B>จังหวัดสุพรรณบุรี</B></h1>
+          <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<B>Suphan Buri</B></h3>
         </div>
       </div>
       <div class="row">
         <div class="col-3" id="sidebar"> 
           <ul>
-            <li><a href="https://lsw-56411.herokuapp.com/website.php">หน้าแรก</a></li>
-            <li><a href="https://lsw-56411.herokuapp.com/page1.php">แหล่งท่องเที่ยว</a></li> 
-            <li><a href="https://lsw-56411.herokuapp.com/page2.php">ประเพณีและวัฒนธรรม</a></li> 
-            <li><a href="https://lsw-56411.herokuapp.com/page3.php">อาหารประจำท้องถิ่น</a></li> 
-            <li><a href="https://lsw-56411.herokuapp.com/page4.php">ผู้จัดทำ</a></li>
+            <li><a href="https://lsw-56411.herokuapp.com/website.php"><B>หน้าแรก</B></a></li>
+            <li><a href="https://lsw-56411.herokuapp.com/page1.php"><B>แหล่งท่องเที่ยว</B></a></li> 
+            <li><a href="https://lsw-56411.herokuapp.com/page2.php"><B>ประเพณีและวัฒนธรรม</B></a></li> 
+            <li><a href="https://lsw-56411.herokuapp.com/page3.php"><B>อาหารประจำท้องถิ่น</B></a></li> 
+            <li><a href="https://lsw-56411.herokuapp.com/page4.php"><B>ผู้จัดทำ</B></a></li>
          </ul>
        </div>
         <div class="col-9" id="content"> 
