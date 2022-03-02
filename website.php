f7b73873b00a8222693ef4f94c19cdf030d56deb
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2022-03-02 15:09:10 +0700

    Update website.php

--- website.php
+++ website.php
@@ -160,16 +160,7 @@
           <h5>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; จังหวัดสุพรรณบุรี มีประชากรรวมทั้งสิ้น 863,347 คน รายได้ประชากร 49,542 บาท/ปี/คน
           </h5>
-    <div class="row">
-        <div class="col-12" id="footer">
-          <!-- ฟุตเตอร์ -->
-          <center>
-            <h3>ผลงานนักเรียน</h3>
-            <hr />
-            <p>รายวิชา WebApplication</p>
-            <p>โรงเรียนหล่มสักวิทยาคม</p>
-          </center>
-        </div>
+    
       </div>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
