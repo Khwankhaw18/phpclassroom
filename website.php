3e8cd99969a30a8353e2e896e76af78b581b0a73
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2022-03-02 14:53:42 +0700

    Update website.php

--- website.php
+++ website.php
@@ -7,7 +7,10 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Suphan Buri Project</title>
-    
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Sarabun"
+    />
     <style>
       #header{
         height: 120px;
