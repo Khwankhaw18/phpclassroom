35a400c6c32a33e45b2731d00a307c73b9e2811a
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2022-03-02 14:48:43 +0700

    Update website.php

--- website.php
+++ website.php
@@ -22,6 +22,11 @@
         height: 0px;
         background-color: #D0F4DE;
       }
+      #footer {
+        padding: 50px;
+        background-color: pink;
+        border-radius: 0 0 15px 15px;
+      }
       ul {
         list-style-type: none;
         margin: 0;
