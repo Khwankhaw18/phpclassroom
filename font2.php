ceb4632a70b6d1947c40145388408ec424c27c2b
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2022-03-03 14:24:44 +0700

    Create font2.php

---
+++ font2.php
@@ -0,0 +1,29 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: myFont;
+        src: url(FCBold.woff);
+      }
+      @font-face{
+        font-family: myFont2;
+        src: url(FCRegular.woff);
+      }
+      #font1{
+        font-family: myFont;
+      }
+      #font2{
+        font-family: myFont2;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="font1">
+      <h1>นางสาวขวัญข้าว เบ้าสกุล เลขที่ 18 ห้อง 5/13</h1>
+    </div>
+    <div id="font2">
+      <h1>นางสาวขวัญข้าว เบ้าสกุล เลขที่ 18 ห้อง 5/13</h1>
+    </div>
+  </body>
+</html>
