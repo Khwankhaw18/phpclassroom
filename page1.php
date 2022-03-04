bb9c473a715377fa4745941d053eaf29e2b23a9f
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2022-03-04 20:17:30 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,27 +1,47 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
     <title>Suphan Buri Project</title>
-   
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Sarabun"
+    />
     <style>
-      #header{
-        height: 180px;
+      body {
+        font-family: "Sarabun";
+        background-color: #fdf2e7;
+      }
+      #container {
+        box-shadow: 6px 4px 8px 6px rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+      }
+      #header {
+        height: 160px;
         background-image: url("https://spbjc.coj.go.th/th/file/get/file/20201008d41d8cd98f00b204e9800998ecf8427e145656.png");
         background-size:100%;
       }
-      #sidebar{
-        height: 0px;
+      #sidebar {
         background-color: #f1f1f1;
         padding: 0px;
       }
-      #content{
-        height: 100px;
-        background-color: #B3DBD8;
+      #content {
+        padding: 10px;
+        background-color: #F5F5F5;
+      }
+      #footer {
+        height: 180px;
+        padding: 50px;
+        background-color: #C0E4F6;
+        border-radius: 0 0 15px 15px;
       }
       ul {
         list-style-type: none;
@@ -39,42 +59,25 @@
       /* Change the link color on hover */
       li a:hover {
         background-color: #555;
-        color: #FFFFFF;
+        color:#FFFFFF;
       }
-    </style> 
-    <style>
-      @font-face{
-        font-family: myFont;
-        src: url(FC%20Bold.woff);
-      }
-      @font-face{
-        font-family: myFont2;
-        src: url(FC%20Regular.woff);
-      }
-      #font1{
-        font-family: myFont;
-      }
-      #font2{
-        font-family: myFont2;
+      p{
+        margin-top: 10px;
       }
     </style>
   </head>
-  <body> 
+  <body style="background-color:#E3EBFD">
+    <!-- คอนเทนเนอร์ -->
     <div class="container">
+      <div id="container">
       <div class="row">
-        <div class="col-12" id="header"
-         <table widht="1300" height="120" align="left"> 
-          <div id="font1">
-            <tr>
-             <br>
-              <h1><font size=6><B></B>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<B>จังหวัดสุพรรณบุรี</B></font></h1>
-              <h3><font size=5><B></B>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<B>Suphan Buri</B></font></h3>
-             </br>
-            </tr>
+        <div class="col-12" id="header">
+          <!-- เฮดเดอร์ -->
         </div>
       </div>
       <div class="row">
-        <div class="col-3" id="sidebar"> 
+        <div class="col-2" id="sidebar">
+          <!-- ไซด์บาร์ -->
           <ul>
             <li><a href="https://lsw-56411.herokuapp.com/website.php"><div id="font1"><B>หน้าแรก</B></a></li>
             <li><a href="https://lsw-56411.herokuapp.com/page1.php"><div id="font1"><B>แหล่งท่องเที่ยว</B></a></li> 
@@ -82,82 +85,72 @@
             <li><a href="https://lsw-56411.herokuapp.com/page3.php"><div id="font1"><B>อาหารประจำท้องถิ่น</B></a></li> 
             <li><a href="https://lsw-56411.herokuapp.com/page4.php"><div id="font1"><B>ผู้จัดทำ</B></a></li>
          </ul>
-       </div>
-        <div class="col-9" id="content"> 
-        <div id="font2">
-        <h5>
-          <br>
-          <font size=5><B>แหล่งท่องเที่ยวสุพรรณบุรี</B></font>
-          <br>
+        </div>
+        <div class="col-10" id="content">
+          <!-- คอนเท้นต์ -->
+          <h4>
+           <font size=5><B>แหล่งท่องเที่ยวสุพรรณบุรี</B></font>
+         </h4>
           <h5>
             <B>1.ตลาดสามชุก : Old Sam Chuk Market</B>
           </h5><br>
-          <table widht="500" height="300" align="center">
-          <tbody><tr>
-           <td>
-             <img src="page1.1.png">
-           </td> 
-          </tr>
-         </tbody></table><br>
+          <center><img src="page1.1.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
              ตลาดสามชุก ตลาดโบราณที่มีมาตั้งแต่สมัยรัชกาลที่5ที่ได้รับการฟื้นฟูจนกลับมาไฉไลอีกครั้งในช่วงเกือบ10ปีที่ผ่านมาจนได้รับรางวัลต่างๆมากมายและกลายเป็นสถานที่ท่องเที่ยวยอดนิยมแห่งหนึ่งของประเทศ ตลาดสามชุกแบ่งพื้นที่ออกทั้งหมดเป็น 4 ซอย มีของจำหน่ายมากมายสารพัด ทั้งข้าวของเครื่องใช้ในชีวิตประจำวันทั่วไป ของประดับ ของที่ระลึกสวยๆเก๋ๆและที่ขาดไม่ได้แน่นอนคือ ของกินมากมายให้เลือกชิม ถ้ามีโอกาสอยากชวนมาสัมผัสกลิ่นอายโบราณแบบร่วมสมัยที่นี่สักครั้ง
           </h5>
           <h5>
             <B>2.อ่างเก็บน้ำหุบเขาวง : Hub Kao Wong Reservoir</B>
           </h5><br>
-              <table widht="500" height="300" align="center">
-          <tbody><tr>
-           <td>
-             <img src="page1.2.png">
-           </td> 
-          </tr>
-         </tbody></table><br>
+              <center><img src="page1.2.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
             สถานที่ท่องเที่ยวเชิงธรรมชาติในพื้นที่ของอำเภอด่านช้าง จ.สุพรรณบุรีนี้เอง และด้วยทำเลที่ตั้งที่ไม่ไกลจากกรุงเทพฯ อีกทั้งที่นี่ยังมีสภาพแวดล้อมที่สวยงามจนได้รับการขนานนามว่า "ปางอุ๋งสุพรรณ" อ่างเก็บน้ำหุบเขาวง เกิดจากการร่วมมือของชุมชน ช่วยกันพัฒนาให้พื้นที่ป่าดังกล่าวอุดมสมบูรณ์และกลายเป็นสถานที่ท่องเที่ยวทางธรรมชาติ และนอกจากนี้ในพื้นที่ของอ่างเก็บน้ำหุบเขาวงยังมีสถานที่ท่องเที่ยวต่างๆ ทั้งพิพิธภัณฑ์ชุมชนให้นักท่องเที่ยวได้มาเที่ยวชมอีกด้วย หรือใครที่อยากมาพักค้างแรมสักคืนที่นี่ก็สามารถกางเต็นท์ และยังมีแพพักไว้รองรับแก่นักท่องเที่ยวอีกด้วย
             </h5>
           <h5>
             <B>3.พิพิธภัณฑ์ลูกหลานพันธุ์มังกร : Dragon Descendants Museum</B>
             </h5><br>
-              <table widht="500" height="300" align="center">
-          <tbody><tr>
-           <td>
-             <img src="page1.3.png">
-           </td> 
-          </tr>
-         </tbody></table><br>
+              <center><img src="page1.3.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
-             ตั้งอยู่ภายในบริเวณศาลเจ้าพ่อหลักเมืองสุพรรณบุรี เขาเล่าว่า “ก่อตั้งขึ้นเพื่อเฉลิมฉลองเนื่องในโอกาสที่ประเทศไทยและสาธารณรัฐประชาชนจีนมีความสัมพันธ์ทางการทูตครบ 20 ปี” พิพิธภัณฑ์ลูกหลานพันธุ์มังกรออกแบบขึ้นเพื่อนำเสนอเรื่องราวประวัติศาสตร์ของอารยธรรมจีนที่ยาวนานถึง 5,000 ปี ซึ่งเป็นที่ยอมรับกันว่าเป็นอารยธรรมที่เก่าแก่และสำคัญที่สุดแห่งหนึ่งของโลก ภายใต้รูปแบบมังกรสัตว์ศักดิ์สิทธิ์ที่รู้จักกันดี 
+            ตั้งอยู่ภายในบริเวณศาลเจ้าพ่อหลักเมืองสุพรรณบุรี เขาเล่าว่า “ก่อตั้งขึ้นเพื่อเฉลิมฉลองเนื่องในโอกาสที่ประเทศไทยและสาธารณรัฐประชาชนจีนมีความสัมพันธ์ทางการทูตครบ 20 ปี” พิพิธภัณฑ์ลูกหลานพันธุ์มังกรออกแบบขึ้นเพื่อนำเสนอเรื่องราวประวัติศาสตร์ของอารยธรรมจีนที่ยาวนานถึง 5,000 ปี ซึ่งเป็นที่ยอมรับกันว่าเป็นอารยธรรมที่เก่าแก่และสำคัญที่สุดแห่งหนึ่งของโลก ภายใต้รูปแบบมังกรสัตว์ศักดิ์สิทธิ์ที่รู้จักกันดี 
           </h5>
           <h5>
             <B>4.หมู่บ้านอนุรักษ์ควายไทย : Buffalo Villages</B>
           </h5><br>
-              <table widht="500" height="300" align="center">
-          <tbody><tr>
-           <td>
-             <img src="page1.4.png">
-           </td> 
-          </tr>
-         </tbody></table><br>
+              <center><img src="page1.4.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
             ไม่มีใครไม่รู้จักควาย แต่คงไม่ใช่ทุกคนที่จะเคยเห็นควายตัวเป็นๆ หมู่บ้านอนุรักษ์ควายไทยจึงเกิดขึ้น บนเนื้อที่ 70 ไร่ โดยจำลองวิถีชีวิตของชุมชนชาวบ้านในแถบชนบท ที่เชื่อมโยงสัมพันธ์กับควายอย่างแยกกันไม่ออก นักท่องเที่ยวจะได้สัมผัสกับการทำนาในแบบโบราณที่ยังใช้แรงงานจากควายและอุปกรณ์การทำนาโบราณ การแสดงความสามารถของควาย และแวะทักทายเจ้าเพื่อนตัวใหญ่สายพันธุ์ดีที่แสนรู้งาน แค่ยกกล้องถ่ายรูปขึ้น เจ้าเพื่อนซี้มีเขาก็จะลุกขึ้นโพสต์ท่าให้เราถ่ายอย่างถนัดถนี่ทีเดียว
           </h5>
           <h5>
             <B>5.บึงฉวากเฉลิมพระเกียรติ : Bueng Chawak</B>
           </h5><br>
-              <table widht="500" height="300" align="center">
-          <tbody><tr>
-           <td>
-             <img src="page1.5.png">
-           </td> 
-          </tr>
-         </tbody></table><br>
+              <center><img src="page1.5.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
             บึงน้ำธรรมชาติขนาดใหญ่กว่า 2,700 ไร่ คาบเกี่ยวระหว่าง อ.หันคา จ.ชัยนาท กับ อ.เดิมบางนางบวช จ.สุพรรณบุรี (อยู่ในเขต อ.เดิมบางนางบวช ประมาณ 1,700 ไร่) ได้รับการพัฒนาจากทางการสุพรรณบุรีให้เป็นสถานที่ท่องเที่ยวหลากรูปแบบ ทั้งสวนสัตว์ ส่วนแสดงพันธุ์สัตว์น้ำ กิจกรรมทางน้ำ และอื่นๆ อีกมากมาย แต่ส่วนที่โดดเด่นที่สุดของบึงฉลากคงหนีไม่พ้นส่วนแสดงพันธุ์สัตว์น้ำ ที่มีอุโมงค์ปลาเป็นจุดขาย ถือเป็นแหล่งท่องเที่ยวที่เหมาะสำหรับครอบครัวจะได้ใช้เวลาร่วมกันได้แทบทั้งวัน สถานแสดงพันธุ์สัตว์น้ำ
           </h5>
         </div>
-
+      </div>
+     <div class="row">
+        <div class="col-12" id="footer">
+          <!-- ฟุตเตอร์ -->
+          <center>
+            <h3><B>ผลงานนักเรียน</B></h3>
+            <hr />
+            <p>รายวิชา WebApplication</p>
+            <p>โรงเรียนหล่มสักวิทยาคม</p>
+          </center>
+        </div>
+       </div>
+     </div>
+   </div>   
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  
-
-</div></div>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
