9b72a54a7402acc3c2160e458b11fc9e2d17f8a5
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2022-03-04 10:41:33 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -63,8 +63,8 @@
       <div class="row">
         <div class="col-12" id="header"align="center">
           <div id="font1">
-          <h1><B>จังหวัดสุพรรณบุรี</B></h1>
-          <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<B>Suphan Buri</B></h3>
+          <h1><font size=10><B>จังหวัดสุพรรณบุรี</B></font></h1>
+          <h3><font size=6><B></B>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<B>Suphan Buri</B></font></h3>
         </div>
       </div>
       <div class="row">
@@ -80,7 +80,7 @@
         <div class="col-9" id="content"> 
         <div id="font2">
         <h5>
-          <B>แหล่งท่องเที่ยวสุพรรณบุรี</B>
+          <font size=5><B>แหล่งท่องเที่ยวสุพรรณบุรี</B></font>
           </h5><br>
           <h5>
             <B>1.ตลาดสามชุก : Old Sam Chuk Market</B>
