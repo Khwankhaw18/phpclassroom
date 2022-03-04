c65afe0ff975c4b1f935037b5bbcbbd3c5d3beb2
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2022-03-04 10:42:13 +0700

    Update website.php

--- website.php
+++ website.php
@@ -63,8 +63,8 @@
       <div class="row">
         <div class="col-12" id="header"align="center">
           <div id="font1">
-          <h1><B>จังหวัดสุพรรณบุรี</B></h1>
-          <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<B>Suphan Buri</B></h3>
+          <h1><font size=10><B>จังหวัดสุพรรณบุรี</B></font></h1>
+          <h3><font size=6><B></B>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<B>Suphan Buri</B></font></h3>
         </div>
       </div>
       <div class="row">
@@ -80,7 +80,7 @@
         <div class="col-9" id="content"> 
          <div id="font2">
          <h4>
-           <B>สุพรรรณบุรี</B>
+           <font size=5><B>สุพรรรณบุรี</B></font>
          </h4>
           <table widht="640" height="440" align="center">
           <tr>
