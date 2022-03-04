6817f5230e2aec68528a6adfda78be65014a68af
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2022-03-04 20:43:04 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,27 +1,47 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
     <title>Suphan Buri Project</title>
-   
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Sarabun"
+    />
     <style>
-      #header{
-        height: 180px;
+      body {
+        font-family: "Sarabun";
+        background-color: #fdf2e7;
+      }
+      #container {
+        box-shadow: 6px 4px 8px 6px rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+      }
+      #header {
+        height: 160px;
         background-image: url("https://spbjc.coj.go.th/th/file/get/file/20201008d41d8cd98f00b204e9800998ecf8427e145656.png");
         background-size:100%;
       }
-      #sidebar{
-        height: 0px;
+      #sidebar {
         background-color: #f1f1f1;
         padding: 0px;
       }
-      #content{
-        height: 0px;
-        background-color: #D0F4DE;
+      #content {
+        padding: 10px;
+        background-color: #F5F5F5;
+      }
+      #footer {
+        height: 180px;
+        padding: 50px;
+        background-color: #C0E4F6;
+        border-radius: 0 0 15px 15px;
       }
       ul {
         list-style-type: none;
@@ -39,42 +59,25 @@
       /* Change the link color on hover */
       li a:hover {
         background-color: #555;
-        color: #FFFFFF;
+        color:#FFFFFF;
       }
-    </style> 
-    <style>
-      @font-face{
-        font-family: myFont;
-        src: url(FC%20Bold.woff);
-      }
-      @font-face{
-        font-family: myFont2;
-        src: url(FC%20Regular.woff);
-      }
-      #font1{
-        font-family: myFont;
-      }
-      #font2{
-        font-family: myFont2;
+      p{
+        margin-top: 10px;
       }
     </style>
   </head>
-  <body> 
+  <body style="background-color:#E3EBFD">
+    <!-- คอนเทนเนอร์ -->
     <div class="container">
+      <div id="container">
       <div class="row">
-        <div class="col-12" id="header"
-         <table widht="1300" height="120" align="left"> 
-          <div id="font1">
-            <tr>
-             <br>
-              <h1><font size=6><B></B>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<B>จังหวัดสุพรรณบุรี</B></font></h1>
-              <h3><font size=5><B></B>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<B>Suphan Buri</B></font></h3>
-             </br>
-            </tr>
+        <div class="col-12" id="header">
+          <!-- เฮดเดอร์ -->
         </div>
       </div>
       <div class="row">
-        <div class="col-3" id="sidebar"> 
+        <div class="col-2" id="sidebar">
+          <!-- ไซด์บาร์ -->
           <ul>
             <li><a href="https://lsw-56411.herokuapp.com/website.php"><div id="font1"><B>หน้าแรก</B></a></li>
             <li><a href="https://lsw-56411.herokuapp.com/page1.php"><div id="font1"><B>แหล่งท่องเที่ยว</B></a></li> 
@@ -82,73 +85,72 @@
             <li><a href="https://lsw-56411.herokuapp.com/page3.php"><div id="font1"><B>อาหารประจำท้องถิ่น</B></a></li> 
             <li><a href="https://lsw-56411.herokuapp.com/page4.php"><div id="font1"><B>ผู้จัดทำ</B></a></li>
          </ul>
-       </div>
-        <div class="col-9" id="content"> 
-        <div id="font2">
-        <h5><br>
-          <font size=5><B>ประเพณีและวัฒนธรรม</B></font>
-          </h5><br>
+        </div>
+        <div class="col-10" id="content">
+          <!-- คอนเท้นต์ -->
+          <h4>
+           <font size=5><B>ประเพณีและวัฒนธรรม</B></font>
+         </h4><br>
           <h5>
-            <B>1.ประเพณีตักบาตรเทโว</B>
+          <B>1.ประเพณีตักบาตรเทโว</B>
           </h5><br>
-          <table widht="500" height="300" align="center">
-          <tbody><tr>
-           <td>
-             <img src="page2.1.png">
-           </td> 
-          </tr>
-         </tbody></table><br>
-          <h5>ประเพณีตักบาตรเทโว จัดขึ้นในวันแรม 1 ค่ำ เดือน 11 เป็นวันคล้ายวันที่พระพุทธเจ้าเสด็จจากเทวโลกลงมาสู่เมืองมนุษย์ จึงนิยมตักบาตรกันเป็นพิเศษ เป็นประเพณีสืบต่อกันมาจนถึงทุกวันนี้ จะมีการนำอาหาร ขนมโดยเฉพาะอย่างยิ่งขนมต้มลูกโยนใส่บาตรถวายแด่พระสงฆ์
-    </h5>
+         <center><img src="page2.1.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
-            <B>2.ประเพณีทิ้งกระจาดสุพรรณบุรี</B>
-          </h5><br>
-              <table widht="500" height="300" align="center">
-          <tbody><tr>
-           <td>
-             <img src="page2.2.png">
-           </td> 
-          </tr>
-         </tbody></table><br>
-          <h5>ประเพณีทิ้งกระจาดสุพรรณบุรี เป็นการเมตตาแก่ดวงวิญญาณที่ล่วงลับไปแล้ว โดยนำสิ่งของต่างๆที่ผู้ตายใช้สอยและสิ่งของจำเป็นอื่นๆมาแจกแก่ผู้ยากจน มีขบวนแห่อัญเชิญเจ้าพ่อหลักเมืองจากที่ประดิษฐานมาวนรอบเมืองมาตั้งไว้ที่ด้านหน้าสมาคมจีนตงฮั้วฮ่วยก้วง</h5>
+            ประเพณีตักบาตรเทโว จัดขึ้นในวันแรม 1 ค่ำ เดือน 11 เป็นวันคล้ายวันที่พระพุทธเจ้าเสด็จจากเทวโลกลงมาสู่เมืองมนุษย์ จึงนิยมตักบาตรกันเป็นพิเศษ เป็นประเพณีสืบต่อกันมาจนถึงทุกวันนี้ จะมีการนำอาหาร ขนมโดยเฉพาะอย่างยิ่งขนมต้มลูกโยนใส่บาตรถวายแด่พระสงฆ์
+       </h5>
+          <h5>
+          <h5>
+          <B>2.ประเพณีทิ้งกระจาดสุพรรณบุรี</B>
+         <center><img src="page2.2.png" class="img-fluid rounded"></center>
+          <br>
+          <h5>
+            ประเพณีทิ้งกระจาดสุพรรณบุรี เป็นการเมตตาแก่ดวงวิญญาณที่ล่วงลับไปแล้ว โดยนำสิ่งของต่างๆที่ผู้ตายใช้สอยและสิ่งของจำเป็นอื่นๆมาแจกแก่ผู้ยากจน มีขบวนแห่อัญเชิญเจ้าพ่อหลักเมืองจากที่ประดิษฐานมาวนรอบเมืองมาตั้งไว้ที่ด้านหน้าสมาคมจีนตงฮั้วฮ่วยก้วง
+          </h5>
           <h5>
             <B>3.งานอนุสรณ์ดอนเจดีย์</B>
             </h5><br>
-              <table widht="500" height="300" align="center">
-          <tbody><tr>
-           <td>
-             <img src="page2.3.png">
-           </td> 
-          </tr>
-         </tbody></table><br>
-          <h5>งานอนุสรณ์ดอนเจดีย์ จัด ณ บริเวณพระบรมราชานุสรณ์ดอนเจดีย์ อำเภอดอนเจดีย์ มีการแสดงยุทธหัตถีชนช้างเทิดพระเกียรติ การออกร้านจำหน่ายสินค้าของอำเภอและหน่วยราชการต่าง ๆ รวมทั้งการแสดงมหรสพ งานนี้จะจัดในช่วงปลายเดือนมกราคม ระยะเวลาประมาณ 9 วัน</h5>
+              <center><img src="page2.3.png" class="img-fluid rounded"></center>
+          <br>
+          <h5>
+            งานอนุสรณ์ดอนเจดีย์ จัด ณ บริเวณพระบรมราชานุสรณ์ดอนเจดีย์ อำเภอดอนเจดีย์ มีการแสดงยุทธหัตถีชนช้างเทิดพระเกียรติ การออกร้านจำหน่ายสินค้าของอำเภอและหน่วยราชการต่าง ๆ รวมทั้งการแสดงมหรสพ งานนี้จะจัดในช่วงปลายเดือนมกราคม ระยะเวลาประมาณ 9 วัน
+       </h5>
           <h5>
             <B>4.ทุ่งทานตะวันสุพรรณบุรี</B>
           </h5><br>
-              <table widht="500" height="300" align="center">
-          <tbody><tr>
-           <td>
-             <img src="page2.4.png">
-           </td> 
-          </tr>
-         </tbody></table><br>
-          <h5>ทุ่งทานตะวันสุพรรณบุรี กำหนดจัดขึ้นในระหว่างวันที่ 1 – 15 ธันวาคม ของทุกปี ณ ศูนย์พันธุ์พืชเพาะเลี้ยง ตำบลพลับพลาไชย อำเภออู่ทอง ตื่นตาตื่นใจกับทุ่งทานตะวันบาน และทุ่งปอเทืองเหลืองอร่าม ไม้ดอกไม้ประดับที่สำคัญทางเศรษฐกิจ</h5>
+             <center><img src="page2.4.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
-           <B>5.ประเพณีแห่เทียนพรรษาสุพรรณบุรี</B> 
+            ทุ่งทานตะวันสุพรรณบุรี กำหนดจัดขึ้นในระหว่างวันที่ 1 – 15 ธันวาคม ของทุกปี ณ ศูนย์พันธุ์พืชเพาะเลี้ยง ตำบลพลับพลาไชย อำเภออู่ทอง ตื่นตาตื่นใจกับทุ่งทานตะวันบาน และทุ่งปอเทืองเหลืองอร่าม ไม้ดอกไม้ประดับที่สำคัญทางเศรษฐกิจ
+        </h5>
+          <h5>
+            <B>5.ประเพณีแห่เทียนพรรษาสุพรรณบุรี</B> 
           </h5><br>
-              <table widht="500" height="300" align="center">
-          <tbody><tr>
-           <td>
-             <img src="page2.5.png">
-           </td> 
-          </tr>
-         </tbody></table><br>
-          <h5>ประเพณีแห่เทียนพรรษาสุพรรณบุรี จัดขึ้นเพื่อเป็นการฟื้นฟูประเพณี วัฒนธรรมทางศาสนา และการนำเอกลักษณ์ที่โดดเด่นของโบราณสถาน โบราณวัตถุ วิถีชีวิตของชนเผ่าต่างๆ ที่อยู่ในจังหวัดสุพรรณบุรี ตลอดจนแหล่งท่องเที่ยวต่างๆ ที่สำคัญใน 10 อำเภอของจังหวัดสุพรรณบุรี
+        <center><img src="page2.5.png" class="img-fluid rounded"></center>
+          <br>
+          <h5>
+            ประเพณีแห่เทียนพรรษาสุพรรณบุรี จัดขึ้นเพื่อเป็นการฟื้นฟูประเพณี วัฒนธรรมทางศาสนา และการนำเอกลักษณ์ที่โดดเด่นของโบราณสถาน โบราณวัตถุ วิถีชีวิตของชนเผ่าต่างๆ ที่อยู่ในจังหวัดสุพรรณบุรี ตลอดจนแหล่งท่องเที่ยวต่างๆ ที่สำคัญใน 10 อำเภอของจังหวัดสุพรรณบุรี
           </h5>
         </div>
-
+      </div>
+      <div class="row">
+        <div class="col-12" id="footer">
+          <!-- ฟุตเตอร์ -->
+          <center>
+            <h3><B>ผลงานนักเรียน</B></h3>
+            <hr />
+            <p>รายวิชา WebApplication</p>
+            <p>โรงเรียนหล่มสักวิทยาคม</p>
+          </center>
+        </div>
+       </div>
+     </div>
+   </div>   
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  
-
-</div></div></body>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
