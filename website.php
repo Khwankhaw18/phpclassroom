e4040538d0d101d01201558609f0d9db506652a6
Khwankhaw18 <89380994+Khwankhaw18@users.noreply.github.com>
2022-03-04 17:07:35 +0700

    Update website.php

--- website.php
+++ website.php
@@ -1,18 +1,18 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8"/>
+    <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- Bootstrap CSS -->
-    <link 
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
-          rel="stylesheet" 
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossorigin="anonymous"
-     />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
     <title>Suphan Buri Project</title>
-   <link
+    <link
       rel="stylesheet"
       href="https://fonts.googleapis.com/css?family=Sarabun"
     />
@@ -24,22 +24,22 @@
       #container {
         box-shadow: 6px 4px 8px 6px rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
       }
-      #header{
+      #header {
         height: 180px;
         background-image: url("https://spbjc.coj.go.th/th/file/get/file/20201008d41d8cd98f00b204e9800998ecf8427e145656.png");
         background-size:100%;
       }
-      #sidebar{
+      #sidebar {
         background-color: #f1f1f1;
         padding: 0px;
       }
-      #content{
-        height: 10px;
-        background-color: #ffffff;
+      #content {
+        padding: 10px;
+        background-color: white;
       }
       #footer {
         padding: 50px;
-        background-color: #AAD4E0;
+        background-color: #AAAD4E0;
         border-radius: 0 0 15px 15px;
       }
       ul {
@@ -63,8 +63,8 @@
       p{
         margin-top: 10px;
       }
-    </style> 
-    <style>
+    </style>
+        <style>
       @font-face{
         font-family: myFont;
         src: url(FC%20Bold.woff);
@@ -81,38 +81,40 @@
       }
     </style>
   </head>
-  
-  <body style="background-color:#ffffff">
+  <body>
     <!-- คอนเทนเนอร์ -->
     <div class="container">
       <div id="container">
       <div class="row">
-        <div class="col-12" id="header"
-
-         <table widht="1300" height="120" align="left"> 
+        <div class="col-12" id="header">
+          <!-- เฮดเดอร์ -->         
+          <h1>Header</h1>
+          <table widht="1300" height="120" align="left"> 
           <div id="font1">
             <tr>
              <br>
               <h1><font size=6><B></B>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<B>จังหวัดสุพรรณบุรี</B></font></h1>
-              <h3><font size=5><B></B>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<B>Suphan Buri</B></font></h3>
-             </br>
+              <h3><font size=5><B></B>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
+              </br>
             </tr>
         </div>
       </div>
       <div class="row">
-        <div class="col-2" id="sidebar"> 
-          
+        <div class="col-2" id="sidebar">
+          <!-- ไซด์บาร์ -->
           <ul>
             <li><a href="https://lsw-56411.herokuapp.com/website.php"><div id="font1"><B>หน้าแรก</B></a></li>
             <li><a href="https://lsw-56411.herokuapp.com/page1.php"><div id="font1"><B>แหล่งท่องเที่ยว</B></a></li> 
             <li><a href="https://lsw-56411.herokuapp.com/page2.php"><div id="font1"><B>ประเพณีและวัฒนธรรม</B></a></li> 
             <li><a href="https://lsw-56411.herokuapp.com/page3.php"><div id="font1"><B>อาหารประจำท้องถิ่น</B></a></li> 
             <li><a href="https://lsw-56411.herokuapp.com/page4.php"><div id="font1"><B>ผู้จัดทำ</B></a></li>
-         </ul>
-       </div>
-        <div class="col-10" id="content"> 
+          </ul>
+        </div>
+        <div class="col-10" id="content">
           <!-- คอนเท้นต์ -->
-         <div id="font2">
+          <center>
+            <h1>Content</h1>
+          <div id="font2">
          <h4>
            <br>
            <font size=5><B>สุพรรรณบุรี</B></font>
@@ -126,7 +128,7 @@
           </tr>
          </table>
             <h5>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;เป็นจังหวัดหนึ่งในภาคกลางของประเทศไทย ห่างจากกรุงเทพมหานคร 107 กิโลเมตร จังหวัดที่อยู่ติดกัน (จากทิศเหนือ วนตามเข็มนาฬิกา) ได้แก่ อุทัยธานี ชัยนาท สิงห์บุรี อ่างทอง พระนครศรีอยุธยา นครปฐม และกาญจนบุรี
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;เป็นจังหวัดหนึ่งในภาคกลางของประเทศไทย ห่างจากกรุงเทพมหานคร 107 กิโลเมตร จังหวัดที่อยู่ติดกัน (จากทิศเหนือ วนตามเข็มนาฬิกา) ได้แก่ อุทัยธานี ชัยนาท สิงห์บุรี อ่างทอง พระนครศรีอยุธยา นครปฐม และกาญจนบุรี
             </h5>
            <br></br>
           <h4>
@@ -136,7 +138,7 @@
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; จังหวัดสุพรรณบุรี ตั้งอยู่ภาคกลางของประเทศไทย ห่างจากกรุงเทพฯ 110 กม. มีเนื้อที่ประมาณ 5,258.008 ตร.กม.หรือประมาณ 3,348,755 ไร่
           </h5>
           <h5>
-             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ทิศเหนือ ติดต่อกับ จังหวัดอุทัยธานี จังหวัดชัยนาท
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ทิศเหนือ ติดต่อกับ จังหวัดอุทัยธานี จังหวัดชัยนาท
           </h5>
           <h5>
              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ทิศใต้ ติดต่อกับ จังหวัดนครปฐมทิศตะวันออก ติดต่อกับ จังหวัดสิงห์บุรี จังหวัดอ่างทอง จังหวัดพระนครศรีอยุธยา
@@ -146,8 +148,8 @@
           </h5>
           <br></br>
           <h5 align="center"><font size=4.5><B>แผนที่จังหวัด</B></font></h5>
-          
-          <table widht="300" height="400" align="center">
+           </center>
+           <table widht="300" height="400" align="center">
           <tr>
            <td>
              <img src="หน้าแรก2.png">
@@ -184,7 +186,7 @@
                     </li>
               <li>  
                 อำเภอสองพี่น้อง
-                  </li>
+</li>
               <li>         
                 อำเภอสามชุก          
                   </li>
@@ -208,7 +210,7 @@
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; จังหวัดสุพรรณบุรี มีประชากรรวมทั้งสิ้น 863,347 คน รายได้ประชากร 49,542 บาท/ปี/คน
           </h5>
 <div class="row">
-        <div class="col-12" id="footer">
+<div class="col-12" id="footer">
           <!-- ฟุตเตอร์ -->
           <center>
             <h3>ผลงานนักเรียน</h3>
@@ -219,10 +221,6 @@
         </div>
     </div>  
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script 
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
-            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
-            crossorigin="anonymous">
-  </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
